7d14aa171804094d68b272315a47e75a22f882c1
jk <47693+sectore@users.noreply.github.com>
2023-11-14 22:06:29 +0100

    Theme (persistent) (#3)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
-import Alpine from 'alpinejs'
 import './styles.css'
+
+import Alpine from 'alpinejs'
+import persist from '@alpinejs/persist'
  
+Alpine.plugin(persist)
+
+const darkModeStore = {
+  on: false, 
+  init() {
+    this.on = Alpine.$persist(
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+    ).as('darkMode') as unknown as boolean; // override type
+  },
+  toggle() {
+    this.on = !this.on
+  }
+}
+
+Alpine.store('darkMode', darkModeStore)
+
 window.Alpine = Alpine
- 
+
 Alpine.start()
